eb5f860239d17a8a13ba82b1a293c267ec0361e1
“anisio2” <anisio2@gmail.com>
2021-05-31 15:46:08 -0300

    first commit

--- script.js
+++ script.js
@@ -1,40 +1,79 @@
-// 1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
-
-// 2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
-
-// 3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
-
-// *** Exercícios bônus
-// 4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
-
-// 5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
-
-let idade = 20
-let humano = "true"
-if (idade > 18){
-    console.log("Você é maior de idade")
-    console.log( "Vc é humano", humano)
-} else{
-    console.log("Rala daqui menor de idade")
+// Crie um objeto que receba ao menos três propriedades sobre você.
+let stormtrooper = {
+    nome: 'Anisio',
+    batalhas: 'Guerra Civil Galáctica'
+    centro_comando: 'Estrela da Morte',
+    estrutura: 'Primeira Ordem',
+    planeta: 'Império Galático',
 }
 
-let mes = 12 //true
-if ((mes === 12) || (mes === 01)){
-    console.log("Você FAZ aniversário em Dezembro ou Janeiro")
-} else{
-    console.log("Você não faz aniversário nesses meses")
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+let stormtrooper = {
+    nome: 'Anisio',
+    batalhas: 'Guerra Civil Galáctica'
+    centro_comando: 'Estrela da Morte',
+    estrutura: 'Primeira Ordem',
 }
+console.log(stormtrooper)
+stormtrooper.planeta = 'Império Galático'
 
-let nome = "Anisio"
-if(nome.substring(0,1) === "R"){
-    console.log("Seu nome começa com R, parabéns!")
-} else{
-    console.log("Seu nome NÃO começa com R, seu nome é", nome)
+// Remova uma propriedade desse objeto.
+let stormtrooper = {
+    nome: 'Anisio',
+    batalhas: 'Guerra Civil Galáctica'
+    centro_comando: 'Estrela da Morte',
+    estrutura: 'Primeira Ordem',
 }
+console.log(stormtrooper)
+delete stormtrooper.centro_comando
 
-let lastName  = "Borba"
-if ((lastName.length > 6) || (lastName.substring(0,1) === "E")){
-    console.log("Seu nome tem mais de 6 letras")
-} else{
-    console.log("Seu nome NÃO tem mais de 6 letras")
+//Mostre no console todas as propriedades desse objeto.
+let stormtrooper = {
+    nome: 'Anisio',
+    batalhas: 'Guerra Civil Galáctica'
+    centro_comando: 'Estrela da Morte',
+    estrutura: 'Primeira Ordem',
 }
+console.log(stormtrooper)
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+// Mostre no console o nome de um amigo de cada lista.
+let cadastro = [{
+    nome:'Darth Vader',
+    idade: 30,
+    telefone:'9999-1234',
+    amigos:['Darth Sidious', 'Darth Sion', 'Larad Noon', 'Bastila Shan' , 'Darth Traya']
+},
+{
+    nome:'C-3PO',
+    idade: 300,
+    telefone:'9999-3030',
+    amigos:['R2-D2', 'Padmé Amidala', 'Han Solo' , 'Anakin Skywalker']
+},
+{
+    nome:'Han Solo',
+    idade: 40,
+    telefone:'9999-4321',
+    amigos:['Chewbacca', 'Mestre Yoda' , 'Ben Solo' , 'Princesa Leia']
+},
+{
+    nome:'R2-D2',
+    idade: 300,
+    telefone:'9999-9999',
+    amigos:['C-3PO', 'Anakin Skywalker', 'Padmé Amidala' , 'Obi-Wan Kenobi']
+},
+{                
+    nome:'Chewbacca',
+    idade: 50,
+    telefone:'9999-3131',
+    amigos:['Conde Dookan', 'Han Solo', 'Anakin Skywalker' , 'C-3PO']
+}]
+
+console.log(cadastro)
+console.log(cadastro[0].amigos[0])
+console.log(cadastro[1].amigos[1])
+console.log(cadastro[2].amigos[2])
+console.log(cadastro[3].amigos[3])
+console.log(cadastro[4].amigos[0])
